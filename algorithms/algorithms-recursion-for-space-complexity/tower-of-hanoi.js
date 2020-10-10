617da07d11ab07fbdd320ef3d3a75821b081beb4
Marius Banea <marius@w-d-s.co.uk>
2020-10-10 04:24:15 +0100

    Tower of Hanoi update

--- algorithms/algorithms-recursion-for-space-complexity/tower-of-hanoi.js
+++ algorithms/algorithms-recursion-for-space-complexity/tower-of-hanoi.js
@@ -1,5 +1,12 @@
 // Tower of Hanoi
-// The goal of the puzzle is to move the entire stack of rods to another rod (can't be the original rod where it was stacked before) where it will be stacked in the ascending order as well. This should be done obeying the following rules: i) Only one disk may be moved at a time ii) Each move consists of taking the upper disk from one of the rods and sliding it onto another rod, on top of the other disks that may already be present on that rod. iii) A larger disk may not placed on top of a smaller disk
+
+/* The goal of the puzzle is to move the entire stack of rods to another rod (can't be the original rod where it was stacked before) where it will be stacked in the ascending order as well.
+
+This should be done obeying the following rules:
+i) Only one disk may be moved at a time
+ii) Each move consists of taking the upper disk from one of the rods and sliding it onto another rod, on top of the other disks that may already be present on that rod.
+iii) A larger disk may not placed on top of a smaller disk
+*/
 
 // signature of fn is num of params, types,
 
@@ -13,13 +20,13 @@ const towerOfHanoi = (height, sourceRod, destinationRod, auxRod) => {
     console.log(`Move disk from ${sourceRod} to ${destinationRod}`);
     towerOfHanoi(height - 1, auxRod, destinationRod, sourceRod);
   };
-  
+
   // Function call:
   console.log(`\n3 disks:`);
   towerOfHanoi(3, "A", "C", "B");
-  
+
   console.log(`\n4 disks:`);
   towerOfHanoi(4, "A", "C", "B");
-  
+
   console.log(`\n5 disks:`);
   towerOfHanoi(5, "A", "C", "B");
