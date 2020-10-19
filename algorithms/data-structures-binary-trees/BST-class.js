2f7c9b7fe55f6f3711a9623139f4564184233402
Sacha Sedriks <sachased@gmail.com>
2020-10-19 15:17:37 -0700

    Updated BST keywords: 'value' for 'val' and 'insert' for 'add'.

--- algorithms/data-structures-binary-trees/BST-class.js
+++ algorithms/data-structures-binary-trees/BST-class.js
@@ -1,17 +1,17 @@
 class BinarySearchTree {
-    constructor(key = null, val = null, parent = null) {
+    constructor(key = null, value = null, parent = null) {
       this.key = key;
-      this.val = val;
+      this.value = value;
       this.parent = parent;
       this.left = null;
       this.right = null;
     }
   
-    add(key, val) {
+    insert(key, value) {
       // If the tree is empty then this key is the root
       if (this.key === null) {
         this.key = key;
-        this.val = val;
+        this.value = value;
         /* If the tree already exists, then start at the root, 
           and compare it to the key you want to insert.
           If the new key is less than the node's key 
@@ -22,12 +22,12 @@ class BinarySearchTree {
             then we can just instantiate and insert the new node 
             as the left child of that node, passing `this` as the parent */
         if (this.left === null) {
-          this.left = new BinarySearchTree(key, val, this);
+          this.left = new BinarySearchTree(key, value, this);
           /* If the node has an existing left child, 
             then we recursively call the `insert` method 
             so the node is added further down the tree */
         } else {
-          this.left.add(key, val);
+          this.left.insert(key, value);
         }
       }
       // Similarly, if the new key is greater than the node's key
